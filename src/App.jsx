618512b5cb4b99ee2ae6ffa5a4717e240be140db
Showman_Das <Showmandas3216@gmail.com>
2023-04-05 10:33:17 +0600

    react router added to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div>
      <h2 className='text-5xl text-fuchsia-500'>Style with tailwind</h2>
     </div>
   )
